f551d387d3ed8702ff775f7fe7b7655fc81d64d8
Pond <pondwachi.th@gmail.com>
2024-05-09 13:26:44 +0700

    faat: JS of pond

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,18 @@
 //Exercise #1: Variable Creation
 //Start coding here
 
+let name = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sianoi Bang Kruay Bang Kruai"
+
+const restaurant = "Chef's Table by Chef James";
+const billPrice = 15000;
+const openBranch = 1500;
+
+
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+billPrice = 5000;
+openBranch = 2500;
